8baedb98d1712b51316e532c548d5a7a3bb4cad3
Cale Gibbard <cgibbard@gmail.com>
2018-09-05 00:05:46 -0400

    Modernise these instances to use Has' and not ToJSON1/FromJSON1.

--- src/Data/Dependent/Sum/Orphans.hs
+++ src/Data/Dependent/Sum/Orphans.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Data.Dependent.Sum.Orphans where
 
@@ -15,21 +16,19 @@ import Data.Dependent.Sum
 import Data.Some (Some)
 import qualified Data.Some as Some
 
-instance (ForallF ToJSON f, Has ToJSON f, ToJSON1 g) => ToJSON (DSum f g) where
-  toJSON ((f :: f a) :=> (x :: g a))
-    | Dict :: Dict (ToJSON a) <- argDict f
-    = toJSON (toJSON f \\ (instF :: ForallF ToJSON f :- ToJSON (f a)), toJSON1 x)
+instance (ForallF ToJSON f, Has' ToJSON f g) => ToJSON (DSum f g) where
+  toJSON ((f :: f a) :=> (g :: g a))
+    = whichever @ToJSON @f @a (has' @ToJSON @g f (toJSON (f, g)))
 
-instance (ForallF ToJSON f, Has ToJSON f, ToJSON1 g) => ToJSON (DMap f g) where
+instance (ForallF ToJSON f, Has' ToJSON f g) => ToJSON (DMap f g) where
     toJSON = toJSON . DMap.toList
 
-instance (FromJSON (Some f), GCompare f, Has FromJSON f, FromJSON1 g) => FromJSON (DSum f g) where
+instance (FromJSON (Some f), GCompare f, Has' FromJSON f g) => FromJSON (DSum f g) where
   parseJSON x = do
-    (tag, val) <- parseJSON x
-    Some.This (parsedTag :: f a) <- parseJSON tag
-    val' <- case argDict parsedTag of
-      (Dict :: Dict (FromJSON a)) -> parseJSON1 val
-    return $ parsedTag :=> val'
+    (jf, jg) <- parseJSON x
+    Some.This (f :: f a) <- parseJSON jf
+    g <- has' @FromJSON @g f (parseJSON jg)
+    return $ f :=> g
 
-instance (FromJSON (Some f), GCompare f, Has FromJSON f, FromJSON1 g) => FromJSON (DMap f g) where
+instance (FromJSON (Some f), GCompare f, Has' FromJSON f g) => FromJSON (DMap f g) where
     parseJSON = fmap DMap.fromList . parseJSON
