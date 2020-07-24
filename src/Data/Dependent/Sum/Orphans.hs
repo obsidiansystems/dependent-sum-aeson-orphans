64d9715f29bee390983b010ea6d3d38e76d5f3bc
Malte Brandy <malte.brandy@maralorn.de>
2020-07-24 18:59:27 +0200

    Workaround for not available Some constructor
    This should also work with ghc 8.0.2.

--- src/Data/Dependent/Sum/Orphans.hs
+++ src/Data/Dependent/Sum/Orphans.hs
@@ -14,7 +14,7 @@ import Data.Dependent.Map (DMap)
 import Data.GADT.Compare (GCompare)
 import qualified Data.Dependent.Map as DMap
 import Data.Dependent.Sum
-import Data.Some (Some(Some))
+import Data.Some (withSomeM, withSome, Some)
 import qualified Data.Some as Some
 
 instance (ForallF ToJSON f, Has' ToJSON f g) => ToJSON (DSum f g) where
@@ -27,12 +27,12 @@ instance (ForallF ToJSON f, Has' ToJSON f g) => ToJSON (DMap f g) where
 instance (FromJSON (Some f), Has' FromJSON f g) => FromJSON (DSum f g) where
   parseJSON x = do
     (jf, jg) <- parseJSON x
-    Some (f :: f a) <- parseJSON jf
-    g <- has' @FromJSON @g f (parseJSON jg)
-    return $ f :=> g
+    withSomeM (parseJSON jf) $ \(f :: f a) -> do
+      g <- has' @FromJSON @g f (parseJSON jg)
+      return $ f :=> g
 
 instance (FromJSON (Some f), GCompare f, Has' FromJSON f g) => FromJSON (DMap f g) where
     parseJSON = fmap DMap.fromList . parseJSON
 
 instance (ForallF ToJSON r) => ToJSON (Some r) where
-  toJSON (Some (x :: r a)) = whichever @ToJSON @r @a (toJSON x)
+  toJSON some = withSome some $ \(x :: r a) -> whichever @ToJSON @r @a (toJSON x)
